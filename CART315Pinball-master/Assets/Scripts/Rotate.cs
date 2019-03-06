4f29d2a1783a13f7ffc4f1bb9df2125ecf062f5f
Ahipsagh <31903660+Ahipsagh@users.noreply.github.com>
2019-03-06 08:29:19 -0500

    Playtest2 - created folders
    Playtest2 - created folders, colour, general organization

---
+++ CART315Pinball-master/Assets/Scripts/Rotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotatespeed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.localScale.y > 0 )
+        {
+            transform.localScale -= new Vector3(0, 0, 0.01f);
+
+        }
+        else
+        {
+            transform.localScale += new Vector3(0, 0, 10.1f);
+
+        }
+    }
+}
