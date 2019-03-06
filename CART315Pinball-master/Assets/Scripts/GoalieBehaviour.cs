36969d5400981d19df953e0b71a905bac3b1c993
Ahipsagh <31903660+Ahipsagh@users.noreply.github.com>
2019-03-06 11:30:51 -0500

    tried command S and Save Project
    tried command S and Save Project

---
+++ CART315Pinball-master/Assets/Scripts/GoalieBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalieBehaviour : MonoBehaviour
+{
+    private float direction = 0f;
+    private bool swtichDirection = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, -0.9f, direction);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(0, -0.9f, direction);
+        if (!swtichDirection)
+        {
+            direction += 0.01f;
+        }
+        else
+        {
+            direction -= 0.01f;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Trigger");
+        if(collision.gameObject.tag == "GoalPostLeft")
+        {
+            swtichDirection = true;
+        }
+        if (collision.gameObject.tag == "GoalPostRight")
+        {
+            swtichDirection = false;
+        }
+    }
+}
