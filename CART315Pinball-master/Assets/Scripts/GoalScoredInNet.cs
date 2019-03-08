d61730f7172ecd4e0da6e5e9914fd2de4313d7b5
Ahipsagh <31903660+Ahipsagh@users.noreply.github.com>
2019-03-08 15:35:31 -0500

    Added Goalie Net, Goalie save reduction in score
    Added Goalie Net, Goalie save reduction in score

---
+++ CART315Pinball-master/Assets/Scripts/GoalScoredInNet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScoredInNet : MonoBehaviour
+{
+    private Vector3 initialposition;
+    public GameObject theball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialposition = theball.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject == theball)
+        {
+            Debug.Log("Goal Scored");
+            theball.transform.position = initialposition;
+            theball.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+    }
+}
