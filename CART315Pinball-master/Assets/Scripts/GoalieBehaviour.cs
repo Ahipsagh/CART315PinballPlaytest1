0f86588351aef81b55cb5dc997590481f5812064
Ahipsagh <31903660+Ahipsagh@users.noreply.github.com>
2019-03-08 14:54:44 -0500

    Goal Post moving and Goalie behaviour scripts
    Goal Post moving and Goalie behaviour scripts

--- CART315Pinball-master/Assets/Scripts/GoalieBehaviour.cs
+++ CART315Pinball-master/Assets/Scripts/GoalieBehaviour.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class GoalieBehaviour : MonoBehaviour
 {
-    private float direction = 0f;
+    float HALF_GOALIE = 2.0f;
+
+    private float direction = 0.0f;
     private bool swtichDirection = false;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, -0.9f, direction);
+        transform.position = new Vector3(0.0f, -0.9f, direction + HALF_GOALIE);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = new Vector3(0, -0.9f, direction);
+        //transform.position = new Vector3(0, -0.9f, direction);
+        transform.position += new Vector3(0.0f, 0.0f, direction);
         if (!swtichDirection)
         {
-            direction += 0.01f;
+            direction += 0.001f;
         }
         else
         {
-            direction -= 0.01f;
+            direction -= 0.001f;
         }
 
     }
@@ -30,7 +33,7 @@ public class GoalieBehaviour : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         print("Trigger");
-        if(collision.gameObject.tag == "GoalPostLeft")
+        if (collision.gameObject.tag == "GoalPostLeft")
         {
             swtichDirection = true;
         }
--- CART315Pinball-master/Assets/Scripts/Rotate.cs
+++ CART315Pinball-master/Assets/Scripts/Rotate.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    float MIN_SIZE = 2.0f;
+    float MAX_SIZE = 5.0f;
+
     public float rotatespeed = 3.5f;
+    private int dir = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-           
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (transform.localScale.y > 0 )
-        {
-            transform.localScale -= new Vector3(0, 0, 0.01f);
+        transform.localScale += new Vector3(0.0f, 0.0f, 0.01f * dir);
 
+        if (transform.localScale.z > MAX_SIZE)
+        {
+            dir = -1;
         }
-        else
+        else if (transform.localScale.z < MIN_SIZE)
         {
-            transform.localScale += new Vector3(0, 0, 10.1f);
-
+            dir = 1;
         }
     }
 }
